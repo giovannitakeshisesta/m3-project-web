63ba485c9ce36d120b63c2e93145894028c4e4da
“Giovanni” <giovannitakeshisesta@gmail.com>
2022-05-07 11:38:11 +0200

    .

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -4,7 +4,8 @@ import { getAccessToken, logout } from '../store/AccessTokenStore';
 
 const createHttp = (useAccessToken = false) => {
   const http = axios.create({
-    baseURL: 'http://localhost:3001/api'
+    // baseURL: 'http://localhost:3001/api'
+    baseURL: 'https://ticketeazy.onrender.com/api'
   })
 
   // ------------------------------------------------------
